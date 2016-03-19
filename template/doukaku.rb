5d4fdc12de82adb65424353793cd25ad3a47ea85
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-03-19 13:10:45 +0900

    Add templates

---
+++ template/doukaku.rb
@@ -0,0 +1,35 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+
+  gem 'activesupport', require: 'active_support/all'
+
+  gem 'minitest', require: 'minitest/autorun'
+  gem 'minitest-reporters'
+
+  gem 'awesome_print'
+  gem 'tapp'
+
+  gem 'pry'
+  gem 'pry-rescue', require: 'pry-rescue/minitest'
+  gem 'pry-stack_explorer'
+end
+
+def solve(input)
+end
+
+TEST_DATA = <<~EOS
+EOS
+
+Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
+
+describe 'Doukaku' do
+  TEST_DATA.each_line do |test|
+    input, expected = test.scan(/"(.*)", "(.*)"/)[0]
+
+    it input do
+      assert_equal expected, solve(input)
+    end
+  end
+end
