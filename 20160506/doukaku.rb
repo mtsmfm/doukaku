50dc2e6c62eef929fa0191de569f4a608f3805a1
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-05-28 13:45:28 +0900

    Impl

--- 20160506/doukaku.rb
+++ 20160506/doukaku.rb
@@ -16,10 +16,63 @@ gemfile do
   gem 'pry-stack_explorer'
 end
 
+MAP = {
+  1 => %w(A B),
+  2 => %w(B C),
+  3 => %w(C D),
+  4 => %w(D E),
+  5 => %w(E F),
+  6 => %w(F G),
+  7 => %w(G H),
+  8 => %w(H A)
+}
+
 def solve(input)
+  lines, start_rock = input.split(?:)
+  lines = lines.chars.map(&:to_i)
+
+  ng_indexes = []
+
+  rock = start_rock
+  lines.each.with_index do |l, i|
+    if MAP[l].include?(rock)
+      rock = (MAP[l] - [rock]).first
+      ng_indexes << i
+    end
+  end
+
+  dead = []
+
+  puts "start_rock: %s, rock: %s" % [start_rock, rock]
+
+  (?A..?H).each do |man|
+    puts man
+
+    pos = man
+
+    lines.each.with_index.to_a.reverse.each do |l, i|
+      print pos
+
+      if MAP[l].include?(pos)
+        if i.in?(ng_indexes)
+          dead << man
+          break
+        else
+          pos = (MAP[l] - [pos]).first
+        end
+      end
+    end
+
+    dead << man if dead.exclude?(man) && (man == rock || pos == start_rock)
+
+    p dead
+  end
+
+  ((?A..?H).to_a - dead).join
 end
 
 TEST_DATA = <<~EOS
+  test("1228:A", "ADEFG")
 EOS
 
 Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
