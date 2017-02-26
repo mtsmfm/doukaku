f45e3de5c2d66c343f40ae3cff7ff3c569a9cf7f
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-02-26 14:42:14 +0900

    init

---
+++ 20170304/doukaku.rb
@@ -0,0 +1,35 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+
+  gem 'activesupport', require: 'active_support/all'
+
+  gem 'minitest', require: 'minitest/autorun'
+  gem 'minitest-reporters'
+
+  gem 'awesome_print'
+  gem 'tapp'
+
+  gem 'pry'
+  gem 'pry-rescue', require: 'pry-rescue/minitest'
+  gem 'pry-stack_explorer'
+end
+
+def solve(input)
+end
+
+TEST_DATA = <<~EOS
+EOS
+
+Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
+
+describe 'Doukaku' do
+  TEST_DATA.each_line do |test|
+    number, input, expected = test.scan(/(\d+).*"(.*)", "(.*)"/)[0]
+
+    it "##{number}" do
+      assert_equal expected, solve(input)
+    end
+  end
+end
